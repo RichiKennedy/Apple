89fe2cb1133adb7a11c6a5a0aacdb3407905bb76
RichiKennedy <richard.kennedy@hyperisland.se>
2022-12-18 21:03:21 +0100

    feature: create authentication

---
+++ firebase/clientApp.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ2NnFq8iW14-KCnERR2NRZ5aHn0fYeFc",
+  authDomain: "apple-clone-d387a.firebaseapp.com",
+  projectId: "apple-clone-d387a",
+  storageBucket: "apple-clone-d387a.appspot.com",
+  messagingSenderId: "612331316791",
+  appId: "1:612331316791:web:ce0857c4fca0e2a3ee01b2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
