d88ce9327b7bfdb7466e50f26be3ef648bed3041
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-12-28 18:22:03 +0100

    feature: create a user dashboard

--- components/UserDashBoard.tsx
+++ components/UserDashBoard.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { UserType } from "../types/UserType";
+import Header from "./Header";
+import LandingPage from "./LandingPage";
+import WowBanner from "./WowBanner";
 
-const UserDashBoard = () => {
-  return (
-    <div>UserDashBoard</div>
-  )
+interface UserDashBoardProps {
+  user: UserType;
+  modalOpen: boolean;
+  setModalOpen: (arg: boolean) => void;
 }
 
-export default UserDashBoard
+const UserDashBoard = ({
+  user,
+  modalOpen,
+  setModalOpen,
+}: UserDashBoardProps) => {
+  return (
+    <div>
+    <Header user={user} modalOpen={modalOpen} setModalOpen={setModalOpen} />
+    <main className="relative h-[200vh]  ">
+      <LandingPage />
+    </main>
+    <section className="relative z-30 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+        <div className="text-white space-y-10 py-16">
+         <h1 className="text-center text-4xl font-medium tracking-wide md:text-5xl">New Promos!</h1>
+        </div>
+       </section>
+    </div>
+  );
+};
+
+export default UserDashBoard;
