841ae5f721e3d3fe8f16777927382c70b3233600
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-12-28 18:19:42 +0100

    feature: styling up the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,46 @@
 import Image from "next/image";
-import React from "react";
-import { AiFillApple, AiOutlineUser } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiFillApple, AiOutlineUser, AiOutlineSearch, AiOutlineShopping } from "react-icons/ai";
 import { FaUserAlt} from "react-icons/fa";
 import { auth } from "../firebase/clientApp";
 import { UserType } from "../types/UserType";
+import { TbMenu } from "react-icons/tb"
+import Link from "next/link";
 
 interface HeaderProps {
   user?: UserType;
+  modalOpen: boolean
+  setModalOpen: (arg: boolean) => void
 }
 
-const Header = ({ user }: HeaderProps) => {
+const Header = ({ user, modalOpen, setModalOpen }: HeaderProps) => {
   return (
-    <header>
-      <div className="flex w-full  items-center justify-between">
-        <AiFillApple className="cursor-pointer text-3xl opacity-75 hover:opacity-100 ease-in-out duration-150" />
-        {user ? <h1> Welcome {user.displayName}</h1> : <h1>Welcome Guest</h1>}
-        {user ? (
-          <button
-            className="bg-gray-400 p-1 border-slate-900 w-[100px] text-sm hover:scale-105"
-            onClick={() => auth.signOut()}
-          >
-            {" "}
-            SIGN OUT{" "}
-          </button>
-        ) : null}
-        <FaUserAlt className="cursor-pointer text-xl m-1 opacity-75 hover:opacity-100 ease-in-out duration-150" />
+    <header className="fixed h-[45px] top-0 z-20 flex w-full items-center justify-between bg-[#1D1D1F] text-white">
+      <div className="flex items-center justify-center w-1/5 md:hidden ">
+      <TbMenu className="headerIcon" />
       </div>
+      <div className="flex items-center justify-center md:w-1/5 ">
+        <AiFillApple className="headerIcon" />
+    </div>
+    <ul className="hidden md:flex flex-1 items-center justify-center space-x-8 text-sm">
+      <a className="headerLink">Mac</a>
+      <a className="headerLink">iPad</a>
+      <a className="headerLink">iPhone</a>
+      <a className="headerLink">Watch</a>
+      <a className="headerLink">AirPods</a>
+    </ul>
+    <div className="flex items-center justify-center gap-x-4 mr-5 md:mr-0 md:w-1/5">
+      <AiOutlineSearch className="headerIcon hidden md:block" />
+      <Link href="/checkout">
+      <div className="relative rounded-full cursor-pointer">
+        <span className="absolute text-[10px] rounded-full -right-1 -bottom-1 z-50 h-4 w-4 flex items-center justify-center bg-gradient-to-r from-pink-500 to-violet-500">5</span>
+      <AiOutlineShopping className="headerIcon"/>
+      </div>
+      </Link>
+        {!user ?  <FaUserAlt className="headerIcon" /> : 
+         <img onClick={() => setModalOpen(true)} src={user.photoURL} alt={user.displayName} className="cursor-pointer rounded-full w-6 h-6" />
+        }
+        </div>
     </header>
   );
 };
